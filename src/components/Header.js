f49e6c2d7a7396148550750876e62219f6e3c5c9
unknown <codbs54@gmail.com>
2021-06-10 21:54:23 +0900

    css module -> css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import css from './css/Header.module.css'
+import './css/root.css'
 function Header(){
 
 return(
-<header className={css.header}>
-  <h1 className={css.title}>writo-do</h1>
-  <h3 className={css.sub_title}>창작자들의 기억저장소</h3>
+<header>
+  <h1 className='header_title'>writo-do</h1>
+  <h3 className='header_sub_title'>창작자들의 기억저장소</h3>
 </header>
 )
 }
