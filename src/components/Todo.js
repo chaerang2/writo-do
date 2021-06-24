c0b0366df80b9a1322d9bdda6825baf6ca46fdb7
unknown <codbs54@gmail.com>
2021-06-25 04:38:24 +0900

    css 정리

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,10 +9,11 @@ const Todo = ({todo,doneToggle, todoDel}) => {
     setTrash(!trash)
   };
   return (
-    <li onMouseOver={trashToggle} onMouseOut={trashToggle}>
-      <div className={todo.done ? 'doneTodo' : ''} onClick={()=>doneToggle(todo.id)}>동그라미</div>
-      <span>{todo.text}</span>
-      <img className={classNames({todo_delete:trash})} src={deleteImg} onClick={()=>todoDel(todo.id)}></img>
+    <li onMouseOver={trashToggle} onMouseOut={trashToggle} className='todo_entry'>
+      <div className={classNames('todo_toggle',{doneTodo:todo.done})} onClick={()=>doneToggle(todo.id)}></div>
+      <span className='todo_text'>{todo.text}</span>
+      <img className={classNames('trash_img',{todo_delete:trash})} src={deleteImg} onClick={()=>todoDel(todo.id)}></img>
+      {/* <div className={'todo_toggle'+(todo.done ? 'doneTodo' : '')} onClick={()=>doneToggle(todo.id)}></div> */}
     </li>
   )
 }
